07c40f66db7d39cc5ba68ae7797a6fb6c55509ce
Huang Cheng <chuang@thoughtworks.com>
2021-12-11 23:02:27 +0800

    [huang cheng] feat: add Home instead of App

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import NotFound from "./components/NotFound";
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from "./pages/home";
 
 ReactDOM.render(
     <BrowserRouter>
         <Routes>
-            <Route path="/" element={<App/>} />
+            <Route path="/" element={<Home/>} />
             <Route path="*" element={<NotFound />} />
         </Routes>
     </BrowserRouter>,
