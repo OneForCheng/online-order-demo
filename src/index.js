66ae1d9dc0833aa7ad300a57beb11ee997d248f8
Huang Cheng <chuang@thoughtworks.com>
2021-12-11 22:46:06 +0800

    [huang cheng] feat: add react router

--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import NotFound from "./components/NotFound";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<App/>} />
+            <Route path="*" element={<NotFound />} />
+        </Routes>
+    </BrowserRouter>,
   document.getElementById('root')
 );
 
